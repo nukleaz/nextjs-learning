8be2124c16d17d74589b5175328343f18de6d3bf
Sergei <Sergei.stolpovskikh@gmail.com>
2026-07-16 23:33:58 +0300

    feat: added Table and Search to invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,40 @@
-export default function page() {
-    return <p>Invoices Page</p>;
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Search from "@/app/ui/search";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import Table from "@/app/ui/invoices/table";
+
+type PageProps = {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+};
+
+export default async function Page({ searchParams }: PageProps) {
+    const params = await searchParams;
+    const query = params?.query || "";
+    const currentPage = Number(params?.page) || 1;
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..." />
+                <CreateInvoice />
+            </div>
+            <Suspense
+                key={query + currentPage}
+                fallback={<InvoicesTableSkeleton />}
+            >
+                <Table query={query} currentPage={currentPage} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                {/* <Pagination totalPages={totalPages} /> */}
+            </div>
+        </div>
+    );
 }
